ffeb67191330d8c188864a20315fb6e9247fec06
MAYURSINGH2206 <71334517+MAYURSINGH2206@users.noreply.github.com>
2021-10-17 18:57:24 +0530

    Add files via upload

---
+++ MayurSingh_DataStructureAssignmentSolution/QUESTION 1/Building/src/Driver/SkyScraper.java
@@ -0,0 +1,29 @@
+package Driver;
+
+import Main.Floors;
+import java.util.*;
+public class SkyScraper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter total number of floors in building");
+     int n = sc.nextInt();
+     int arr[] = new int[n];
+     int k=1;
+     for(int i=0;i<n;i++)
+     {
+    	 System.out.println("enter floor size given on day :" + k);
+    	 arr[i] = sc.nextInt();
+    	 k++;
+     }
+     
+     Floors obj = new Floors();
+     System.out.println("The order of construction is as follows");
+     
+     obj.Implementation(arr , n);
+    
+     
+	}
+
+}
